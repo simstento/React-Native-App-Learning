3b56d0d2aed4bdc4216e1c967ac1222bb8b70824
Simon <simon.sterner@etu.se>
2026-04-17 21:50:21 +0200

    Added macro files, imported & updated home screen to display

--- macrozone/src/app/(tabs)/index.tsx
+++ macrozone/src/app/(tabs)/index.tsx
@@ -1,19 +1,14 @@
 import { globalStyles } from '@/styles/global';
-import { Link } from 'expo-router';
-import { Text, ScrollView } from 'react-native';
+import { StyleSheet, ScrollView, Text, View } from 'react-native';
 import HomeHeader from '../../components/HomeHeader';
+import MacroGrid from '@/components/MacroGrid';
 
 export default function HomeScreen() {
   return (
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
-     <HomeHeader />
-      <Link href='/meals' style={{ fontSize: 18, color: '#007bff' }}>
-        Go to Meals
-      </Link>
-      <Link href='/add-meal' style={{ fontSize: 18, color: '#007bff', marginTop: 10 }}>
-        Add a Meal
-      </Link>
+      <HomeHeader />
+      <MacroGrid />
     </ScrollView>
   );
 }
